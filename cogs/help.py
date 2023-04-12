5f30c4c52774aca5e4bf00845d945dd4da6b3205
Kian Ahmadian <dev.kian.ahmadian@gmail.com>
2023-04-12 12:40:24 -0700

    Send message when `joins` or `leaved` from group

--- cogs/help.py
+++ cogs/help.py
@@ -21,3 +21,28 @@ class Help:
 				return await message.reply("🚀 * پاسخ دستور فوق در پی وی، داده شد *")
 
 			return await message.author.send('🤖 *به ربات گروه بان خوش آمدید*\n👇 لطفا از طریق دکمه های زیر، کارکرد خود را *انتخاب* نمائید، و یا در صورتی که با ربات آشنایی ندارید بر روی گزینه *"راهنما"* کلیک نمائید.', components=self.bot.components.help_command())
+
+	async def when_user_join_me(self, message: bale.Message, chat: bale.Chat, user: bale.User):
+		if chat.type.is_group_chat() and user.user_id == self.bot.user.user_id:
+			inviter: bale.User = message.from_user
+
+			with self.bot.make_db() as connection:
+				cursor = connection.cursor()
+				cursor.execute("INSERT INTO chat(chat_id) VALUES (%s, )", (chat.chat_id, ))
+				connection.commit()
+				cursor.close()
+
+			try:
+				await chat.send("سلام؛ من ربات گروه بان هستم.\nاحتمالا تعریف من رو زیاد شنیدی 😎\n\n❌ *برای شروع کار با ربات و تشخیص ادمینان گروه، من را ادمین (همه دسترسی ها) نمائید.*\n\n💻 [سایت ربات گروه بان](https://groupban.ir)")
+			except:
+				return await inviter.send("سلام، من ربات گروه بان هستم.\nشما من را عضو گروه کبابی محمد و پسران کرده اید.\n\n❌ *برای شروع کار با ربات و تشخیص ادمینان گروه، من را ادمین (همه دسترسی ها) نمائید.*\n\n[سایت ربات گروه بان](https://groupban.ir)")
+
+	async def when_user_kicked_me(self, message: bale.Message, chat: bale.Chat, user: bale.User):
+		if chat.type.is_group_chat() and user.user_id == self.bot.user.user_id:
+			with self.bot.make_db() as connection:
+				cursor = connection.cursor()
+				cursor.execute("DELETE FROM chat WHERE chat_id = '{}'".format(chat.chat_id))
+				connection.commit()
+				cursor.close()
+
+			return await chat.send("*🤚 خداحافظ {}*\n\n🌟 به جهت بهبود کیفیت بازو در نظرسنجی شرکت کرده و نظر خود را در رابطه با گروه بان اعلام نمائید.\n💻 [سایت ربات گروه بان](https://groupban.ir)\n➕ [دعوت ربات](send:/invite)".format(chat.title))
