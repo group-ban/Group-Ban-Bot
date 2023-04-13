b9ffe82dc6083ce358b0e013f7d96d36f397f2c4
Kian Ahmadian <dev.kian.ahmadian@gmail.com>
2023-04-13 23:55:48 +0430

    Add `exit` button to setup Components

--- utils/components.py
+++ utils/components.py
@@ -7,4 +7,4 @@ class Components:
         return C(keyboards=[[Keyboard("چت های من")], [Keyboard("خرید پریمیوم ربات"), Keyboard("پشتیبانی")], [Keyboard("راهنما")]])
 
     def setup_command(self):
-        return C(inline_keyboards=[[InlineKeyboard("ضد اسپم", callback_data="anti_spam"), InlineKeyboard("ضد منشن", callback_data="anti_mention")], [InlineKeyboard("ضد لینک", callback_data="anti_link"), InlineKeyboard("ضد کلمه", callback_data="anti_word")], InlineKeyboard("پاسخگویی خودکار", callback_data="auto_answer")])
+        return C(inline_keyboards=[[InlineKeyboard("ضد اسپم", callback_data="anti_spam"), InlineKeyboard("ضد منشن", callback_data="anti_mention")], [InlineKeyboard("ضد لینک", callback_data="anti_link"), InlineKeyboard("ضد کلمه", callback_data="anti_word")], InlineKeyboard("پاسخگویی خودکار", callback_data="auto_answer"), InlineKeyboard("ذخیره", callback_data="exit")])
