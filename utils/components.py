ebb67086208a539502ba545c39c90fce2778c539
Kian Ahmadian <dev.kian.ahmadian@gmail.com>
2023-04-15 22:36:49 +0430

    Change Text & Buttons in help command

--- utils/components.py
+++ utils/components.py
@@ -4,7 +4,7 @@ from bale import Keyboard
 
 class Components:
     def help_command(self):
-        return C(keyboards=[[Keyboard("چت های من")], [Keyboard("خرید پریمیوم ربات"), Keyboard("پشتیبانی")], [Keyboard("راهنما")]])
+        return C(inline_keyboards=[[InlineKeyboard("دعوت ربات به گروه", url="https://groupban.ir/invite"), InlineKeyboard("داکیومنت ربات", url="https://groupban.ir/commands")], [InlineKeyboard("خرید پریمیوم ربات", url="https://groupban.ir/premium"), InlineKeyboard("سفارش تبلیغ", url="https://groupban.ir/tabligh")]])
 
     def setup_command(self):
         return C(inline_keyboards=[[InlineKeyboard("ضد اسپم", callback_data="anti_spam"), InlineKeyboard("ضد منشن", callback_data="anti_mention")], [InlineKeyboard("ضد لینک", callback_data="anti_link"), InlineKeyboard("ضد کلمه", callback_data="anti_word")], InlineKeyboard("پاسخگویی خودکار", callback_data="auto_answer"), InlineKeyboard("ذخیره", callback_data="exit")])
