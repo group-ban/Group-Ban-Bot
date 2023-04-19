5ad3caef46be0bc9fccbdf664cc84173e2d5b165
Kian Ahmadian <dev.kian.ahmadian@gmail.com>
2023-04-20 01:28:28 +0430

    Some changes in view of commands

--- cogs/commands.py
+++ cogs/commands.py
@@ -25,7 +25,7 @@ class Commands:
             return await message.chat.send("❌ *تنها امکان ارسال این دستور در گروه امکان پذیر است*")
 
         if message.content == "/commands":
-            return await message.chat.send("⛏ *دستورات ربات*\n\n💠 *دستورات خدمات اصلی*\n\n🔧 [تنظیم کانفیگ کلی ربات](send:/setup)\n🔧 [تنظیم بخش پاسخگویی خودکار](send:/auto_answer)\n🔧 [تنظیم بخش حذف کلمه](send:/bad_words)\n\n💠 *دستورات خدمات فرعی*\n\n🔧 [دریافت اطلاعات گروه](send:/groupinfo)\n\n💡 برای ارسال دستور، کافیست بر روی آن کلیک نمائید.", components=self.bot.components.site_and_support_buttons())
+            return await message.chat.send("⛏ *دستورات ربات*\n\n💠 *دستورات خدمات اصلی*\n\n🔧 [تنظیم کانفیگ کلی ربات](send:/setup)\n🔧 [تنظیم بخش پاسخگویی خودکار](send:/auto_answer)\n🔧 [تنظیم بخش حذف کلمه](send:/anti_word)\n\n💠 *دستورات خدمات فرعی*\n\n🔧 [دریافت اطلاعات گروه](send:/groupinfo)\n\n💡 برای ارسال دستور، کافیست بر روی آن کلیک نمائید.", components=self.bot.components.site_and_support_buttons())
 
         elif message.content == "/groupinfo":
             groupinfo = await self.render_chat_info(message.chat)
@@ -37,4 +37,4 @@ class Commands:
             admins = await chat.get_chat_administrators()
         except:
             admins = None
-        return "👥 *اطلاعات اصلی گروه*\n🆔 شناسه عددی: {0}\nℹ نام: {1}\n💎 پرمیوم گروه بان: ندارد\n\n👮‍♂️ *اطلاعات ادمینان گروه*\n{3}\n\n💻 [سایت گروه بان](https://groupban.ir)".format(chat.chat_id, chat.title, chat.invite_link, "\n".join(["👤 {} | {}".format(admin.user.first_name, admin.status.status) for admin in admins]) if admins else "❌ به دلیل کمبود دسترسی، امکان دریافت ادمین هارا ندارم")
+        return "👥 *اطلاعات اصلی گروه*\n🆔 شناسه یکتا گروه: {0}\nℹ نام: {1}\n\n👮‍♂️ *اطلاعات ادمینان گروه*\n{3}\n\n💻 [سایت گروه بان](https://groupban.ir)".format(chat.chat_id, chat.title, chat.invite_link, "\n".join(["👤 {} | {}".format(admin.user.first_name, admin.status.status) for admin in admins]) if admins else "❌ به دلیل کمبود دسترسی، امکان دریافت ادمین هارا ندارم")
