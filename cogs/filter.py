9bd47849f721bf793305b646dbf085c6a5112212
Kian Ahmadian <dev.kian.ahmadian@gmail.com>
2023-04-22 13:33:43 +0430

    Add Anti Spam for members

--- cogs/filter.py
+++ cogs/filter.py
@@ -1,17 +1,29 @@
 import re
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Dict
 import bale
+from utils import UserRateLimit
 if TYPE_CHECKING:
     from ..bot import GroupBan
 
 class Filter:
     def __init__(self, bot: "GroupBan"):
         self.bot = bot
+        self.rate_limits: Dict[int, Dict[int, "UserRateLimit"]] = {}
 
     def setup(self):
         return {
             self.when_send_message_in_group: "message"
         }
+
+    def chat_member_have_rate_limit(self, message: "bale.Message", chat_id: int, user_id: int) -> bool:
+        ratelimit = self.rate_limits.get(chat_id, {}).get(user_id)
+        if not ratelimit:
+            if not self.rate_limits.get(chat_id):
+                self.rate_limits[chat_id] = {}
+            self.rate_limits[chat_id][user_id] = UserRateLimit()
+            ratelimit = self.rate_limits[chat_id][user_id]
+
+        return ratelimit.user_is_rate_limited(message)
 
     async def when_send_message_in_group(self, message: bale.Message):
         if not message.chat.type.is_group_chat():
@@ -20,11 +32,11 @@ class Filter:
 
         with self.bot.make_db() as connection:
             cursor = connection.cursor()
-            cursor.execute("SELECT anti_link, anti_mention, anti_word, auto_answer FROM chat WHERE chat_id = '{}'".format(message.chat.chat_id))
+            cursor.execute("SELECT anti_spam, anti_link, anti_mention, anti_word, auto_answer FROM chat WHERE chat_id = '{}'".format(message.chat.chat_id))
             filters = cursor.fetchone()
             if not filters:
                 return
-            (anti_link, anti_mention, anti_word, auto_answer) = filters
+            (anti_spam, anti_link, anti_mention, anti_word, auto_answer) = filters
             cursor.execute("SELECT word, answer FROM auto_answer WHERE chat_id = '{}'".format(message.chat.chat_id))
             auto_answer_dict = {word: answer for word, answer in cursor.fetchall()}
             cursor.execute("SELECT word FROM bad_words WHERE chat_id = '{}'".format(message.chat.chat_id))
@@ -36,6 +48,10 @@ class Filter:
             member = None
         if not member or not member.status.is_member():
             return
+
+        user_spam_state = self.chat_member_have_rate_limit(message, int(message.chat_id), int(message.author.user_id))
+        if anti_spam and user_spam_state:
+            return await message.delete()
 
         if (anti_link and re.search("(?P<url>https?://[^\s]+)", standard_content)) or (anti_mention and re.search("(?<=@)\w+", standard_content)):
             return await message.delete()
