7557e299dde34990150d3d33ac0f06b56cff5d9a
Kian Ahmadian <dev.kian.ahmadian@gmail.com>
2023-04-27 13:51:25 +0430

    Make ignore for system message

--- utils/ratelimit.py
+++ utils/ratelimit.py
@@ -7,9 +7,6 @@ class UserRateLimit:
         self.last_date = None
 
     def user_is_rate_limited(self, message: "Message") -> bool:
-        if message.message_id == 0:
-            return False
-
         if self.last_date and self.last_date + timedelta(seconds=3) >= message.date and self.last_message_text == message.content:
             self.last_date = message.date
             self.last_message_text = message.content
