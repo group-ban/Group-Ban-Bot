be126d46437e907b1a435b68ea3fd218f8eddb55
Kian Ahmadian <dev.kian.ahmadian@gmail.com>
2023-04-27 16:37:31 +0430

    Add shield for unknown bots & Change UI in `/groupinfo`

--- cogs/commands.py
+++ cogs/commands.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional, List
 import bale
 
 if TYPE_CHECKING:
@@ -8,7 +8,7 @@ def parse_admin_status(status: bale.ChatMemberStatus):
     if status.is_admin():
         return "ادمین"
     elif status.is_owner():
-        return "سازنده چت"
+        return "💎 *سازنده چت*"
     return status
 
 class Commands:
@@ -32,16 +32,20 @@ class Commands:
             return await message.chat.send(self.bot.base_messages["only_group"])
 
         if message.content == "/commands":
-            return await message.chat.send("⛏ *دستورات ربات*\n\n🔷 بخش گروه\n\n🛠 *تنظیمات کلی*\n\n🔧 [تنظیم قابلیت های ربات](send:/setup)\n🔧 [بخش پاسخگویی خودکار](send:/auto_answer)\n🔧 [بخش حذف کلمه](send:/anti_word)\n\n🔔 *اطلاعات*\n\n🔧 [دریافت مشخصات گروه](send:/groupinfo)\n\n📡 *پشتیبانی*\n\n🔧 [درخواست کارشناس خدمات ربات](send:/helpme)\n\n💡 برای ارسال دستور، کافیست بر روی آن کلیک نمائید.", components=self.bot.components.commands())
+            return await message.chat.send("⛏ *دستورات ربات*\n\n🔷 بخش گروه\n\n🛠 *تنظیمات کلی*\n\n🔧 [تنظیم قابلیت های ربات](send:/setup)\n🔧 [بخش پاسخگویی خودکار](send:/auto_answer)\n🔧 [بخش حذف کلمه](send:/anti_word)\n\n🔔 *اطلاعات*\n\n🔧 [دریافت مشخصات گروه](send:/groupinfo)\n\n🎀 *خوش آمد گو*\n🔒 این بخش تنها برای گروه های دارای پریمیوم گروه بان فعال است\n\n📡 *پشتیبانی*\n\n🔧 [درخواست کارشناس خدمات ربات](send:/helpme)\n\n💡 برای ارسال دستور، کافیست بر روی آن کلیک نمائید.", components=self.bot.components.commands())
 
         elif message.content == "/groupinfo":
-            groupinfo = await self.render_chat_info(message.chat)
+            try:
+                admins = await message.chat.get_chat_administrators()
+            except bale.BaleError:
+                admins = None
+            for admin in admins or []:
+                if admin.bot and self.bot.user != admin.user:
+                    await message.chat.send("🛡 *سپر امنیتی گروه بان: استفاده از ربات های ناشناخته*\n\n استفاده از ربات های دیگر و ادمین کردن آن ها موجب *پایین آمدن امنیت گروه* شماست.\n⚔ ربات های ناشناخته سعی در جلب توجهات داشته و در فرصتی مناسب، اقدام به هدف قرار دادن گروه شما به وسیله اسپم و ... میکنند.\n\n💡 پیشنهاد میکنیم حدالمکان از ادمین کردن آن ها *خودداری* نمائید.\n\n💎 با گروه بان، امنیت اعضای گروهتو تضمین کن!")
+                    break
+            groupinfo = await self.render_chat_info(message.chat, admins)
             return await message.chat.send(groupinfo)
 
-    async def render_chat_info(self, chat: bale.Chat):
+    async def render_chat_info(self, chat: bale.Chat, admins: Optional[List["bale.ChatMember"]]):
         chat = await self.bot.get_chat(chat.chat_id)
-        try:
-            admins = await chat.get_chat_administrators()
-        except bale.BaleError:
-            admins = None
-        return "👥 *اطلاعات اصلی گروه*\n🆔 شناسه یکتا گروه: {}\nℹ نام: {}\n\n👮‍♂️ *اطلاعات ادمینان گروه*\n{}\n\n💻 [سایت گروه بان](https://groupban.ir)".format(chat.chat_id, chat.title, "\n".join(["👤 {} | {}".format(admin.user.first_name, parse_admin_status(admin.status)) for admin in admins]) if admins else "❌ به دلیل کمبود دسترسی، امکان دریافت ادمین هارا ندارم")
+        return "👥 *مشخصات اصلی گروه*\n\nℹ نام گروه: *{}*\n🆔 شناسه یکتای گروه: *{}*\n💡 این شناسه در هنگام برقراری ارتباط با پشتیبانی گروه بان، خرید پریمیوم ربات و ... کاربرد دارد.\n\n👮‍♂️ *مشخصات ادمینان گروه*\n{}".format(chat.chat_id, chat.title, "\n".join(["👤 {} | {}".format(admin.user.first_name, parse_admin_status(admin.status)) for admin in admins]) if admins else "❌ به دلیل کمبود دسترسی، امکان دریافت ادمین هارا ندارم")
