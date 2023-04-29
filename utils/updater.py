97df42472b8a3dbaf3457cfe3ad20165c367b415
Kian Ahmadian <dev.kian.ahmadian@gmail.com>
2023-04-29 23:28:05 +0430

    Improve Code Quality

--- utils/updater.py
+++ utils/updater.py
@@ -9,16 +9,17 @@ class GroupBanUpdater(Updater):
         while self._is_running:
             try:
                 updates = await self.bot.get_updates(offset=self._last_offset)
-                for update in updates:
-                    await self.call_to_dispatch(update)
-
-                if bool(updates):
-                    if not self._last_offset or ( updates[-1].update_id - self._last_offset <= 50 ):
-                        self._last_offset = updates[-1].update_id
-                if self.interval:
-                    await asyncio.sleep(self.interval)
             except Exception as exc:
                 await self.bot.on_error("getUpdates", exc)
+            else:
+                if updates:
+                    for update in updates:
+                        await self.call_to_dispatch(update)
+                    if not self._last_offset or ( updates[-1].update_id - self._last_offset <= 50 ):
+                        self._last_offset = updates[-1].update_id
+
+                if self.interval:
+                    await asyncio.sleep(self.interval)
 
     def stop(self):
         if not self._is_running:
