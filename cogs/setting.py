2ef238086a0493d7870bb8abaa900086e1d177a2
Kian Ahmadian <dev.kian.ahmadian@gmail.com>
2023-04-30 20:59:21 +0430

    Fix `BaleError` to show button in Edit message

--- cogs/setting.py
+++ cogs/setting.py
@@ -56,7 +56,7 @@ class Setting:
 			return await check_message.edit(self.bot.base_messages["miss_permission"])
 		else:
 			if member.status.is_member():
-				return await check_message.edit("❌ *شما ادمین چت نیستید*\n✨ با *اضافه کردن* من به گروهت از این امکان استفاده کن!", components=bale.Components(inline_keyboards=[bale.InlineKeyboard("دعوت گروه بان به چت", url="https://groupban.ir/invite")]))
+				return await check_message.edit("❌ *شما ادمین چت نیستید*\n✨ با * (https://groupban.ir/invite)[اضافه کردن] * من به گروهت از این امکان استفاده کن!")
 
 		return await self.commands.get(message.content)(message, check_message)
 
