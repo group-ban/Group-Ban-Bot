c740cbed896ec5944ab287dda65a47521d0b58f3
Kian Ahmadian <dev.kian.ahmadian@gmail.com>
2023-04-30 22:58:27 +0430

    Add new row Announce channel

--- utils/components.py
+++ utils/components.py
@@ -12,7 +12,7 @@ class Components:
         return C(inline_keyboards=[[InlineKeyboard("ضد اسپم", callback_data="anti_spam"), InlineKeyboard("ضد لینک", callback_data="anti_link")], [InlineKeyboard("ضد منشن", callback_data="anti_mention"), InlineKeyboard("ضد کلمه", callback_data="anti_word")], [InlineKeyboard("ضد بازارسال", callback_data="anti_forward"), InlineKeyboard("پاسخگویی خودکار", callback_data="auto_answer")], InlineKeyboard("ذخیره", callback_data="exit")])
 
     def site_and_support_buttons(self):
-        return C(inline_keyboards=[[InlineKeyboard("سایت گروه بان", url="https://groupban.ir/"), InlineKeyboard("پشتیبانی ربات", url="https://groupban.ir/support")]])
+        return C(inline_keyboards=[InlineKeyboard("اطلاع رسانی گروه بان", url="https://ble.ir/groupban"), [InlineKeyboard("سایت گروه بان", url="https://groupban.ir/"), InlineKeyboard("پشتیبانی ربات", url="https://groupban.ir/support")]])
 
     def about_command(self):
         return C(inline_keyboards=[[InlineKeyboard("گیت هاب گروه بان", url="https://github.com/group-ban")], InlineKeyboard("گیت هاب کتابخانه بله بات (python-bale-bot)", url="https://github.com/python-bale-bot")])
