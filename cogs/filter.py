075769d5f704fdd84c2ce505c31bd83df3d8cdcc
Kian Ahmadian <dev.kian.ahmadian@gmail.com>
2023-05-12 00:17:06 +0330

    Increasing the speed of the Bot

--- cogs/filter.py
+++ cogs/filter.py
@@ -1,5 +1,5 @@
 import re
-from typing import TYPE_CHECKING, Dict
+from typing import TYPE_CHECKING, Dict, Callable
 import bale
 from utils import UserRateLimit
 if TYPE_CHECKING:
@@ -25,6 +25,19 @@ class Filter:
 
         return ratelimit.user_is_rate_limited(message)
 
+    async def do_after_check_chat_member(self, core: "Callable", message: "bale.Message", *args):
+        try:
+            member = await self.bot.get_chat_member(message.chat_id, message.author.user_id)
+        except bale.BaleError:
+            member = None
+        else:
+            if member.status.is_member():
+                member = None
+
+        if member:
+            return await core(*args)
+        return self.bot.dispatch("verified_message", message)
+
     async def when_send_message_in_group(self, message: bale.Message):
         if not message.author or message.message_id == 0:
             return
@@ -43,30 +56,24 @@ class Filter:
             cursor.execute("SELECT word FROM bad_words WHERE chat_id = '{}'".format(message.chat.chat_id))
             bad_words = [word for (word, ) in cursor.fetchall()]
         standard_content = self.bot.make_persian(message.content)
-        try:
-            member = await self.bot.get_chat_member(message.chat_id, message.author.user_id)
-        except bale.BaleError:
-            member = None
-        if not member or not member.status.is_member():
-            return self.bot.dispatch("verified_message", message)
 
         user_spam_state = self.chat_member_have_rate_limit(message, int(message.chat_id), int(message.author.user_id))
         if anti_spam and user_spam_state:
-            return await message.delete()
+            return await self.do_after_check_chat_member(message.delete, message)
 
         if (anti_link and re.search("(?P<url>https?://\S+)", standard_content)) or (anti_mention and re.search("(?<=@)\w+", standard_content)):
-            return await message.delete()
+            return await self.do_after_check_chat_member(message.delete, message)
 
         if anti_forward and message.forward_from_message_id:
-            return await message.delete()
+            return await self.do_after_check_chat_member(message.delete, message)
 
         if anti_word:
             for word in bad_words:
                 if word in standard_content:
-                    await message.delete()
+                    await self.do_after_check_chat_member(message.delete, message)
                     break
 
         if auto_answer and auto_answer_dict.get(standard_content):
-            return await message.chat.send(auto_answer_dict.get(standard_content))
+            return await self.do_after_check_chat_member(message.chat.send, message, auto_answer_dict.get(standard_content))
 
         self.bot.dispatch("verified_message", message)
