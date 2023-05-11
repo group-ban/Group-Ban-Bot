36278d5a7cfb153defd7a573a6d2829e68a87bca
Kian Ahmadian <dev.kian.ahmadian@gmail.com>
2023-05-11 18:56:58 +0430

    Add converter to developers section & Improve Code Quality

--- cogs/developer.py
+++ cogs/developer.py
@@ -1,6 +1,7 @@
 import asyncio
-from typing import TYPE_CHECKING, Optional, List
+from typing import TYPE_CHECKING
 import bale
+from utils import persianNumbers
 
 if TYPE_CHECKING:
     from ..bot import GroupBan
@@ -19,16 +20,22 @@ class Developer:
         }
 
     async def when_send_command(self, message: bale.Message):
+        if message.forward_from_message_id:
+            return
+
+        for (persian_num, english_num) in persianNumbers:
+            message.content = message.content.replace(persian_num, english_num)
+
         if message.content == "/d info":
             return await message.chat.send("✨ هویت شما تائید شد؛ شما یکی از توسعه دهندگان گروه بان هستید")
 
         elif message.content in ["/developer", "/d"]:
-            return await message.chat.send("✨ *توسعه دهندگان*\n\n⛏ دریافت لیست چت های ثبت شده\n/d group list\n\n⛏ ارسال پیام به چت (الزاما ثبت شده)\n/d group fetch <chat id>\n\n⛏ دریافت گروه (ثبت شده یا ثبت نشده)\n/d group send <chat id>\n\n⛏ دریافت آیدی کاربر (با یکی از پیام های وی)\n/d user get\n\n⛏ بن کردن فرد در گروه خاص (الزاما گروه ثبت شده)\n/d group ban <chat id> <member id>\n\n🚀 گروه بان با شما توسعه دهندگان گرامی، *گـــروه بـــان* شده است.")
+            return await message.chat.send("✨ *توسعه دهندگان*\n\n⛏ دریافت لیست چت های ثبت شده\n/d group list\n\n⛏ ارسال پیام به چت (الزاما ثبت شده)\n/d group fetch <chat id>\n\n⛏ دریافت گروه (ثبت شده یا نشده)\n/d group send <chat id>\n\n⛏ دریافت آیدی کاربر (با یکی از پیام های وی)\n/d user get\n\n⛏ خارج شدن از چت (ثبت شده یا نشده)\n/d group leave <chat id>\n\n⛏ بن کردن فرد در گروه خاص (الزاما گروه ثبت شده)\n/d group ban <chat id> <member id>\n\n🚀 گروه بان با شما توسعه دهندگان گرامی، *گـــروه بـــان* شده است.")
 
         elif message.content == "/d group list":
             with self.bot.make_db() as connection:
                 cursor = connection.cursor()
-                cursor.execute("SELECT chat_id FROM chat LIMIT 100")
+                cursor.execute("SELECT chat_id FROM chat LIMIT 200")
                 chats = cursor.fetchall()
                 return await message.chat.send("💎 *لیست چت ها*\n\n{}".format("\n".join([f"🔧 {chat_id}" for (chat_id, ) in chats])))
 
@@ -43,19 +50,36 @@ class Developer:
             groupinfo = await render_chat_info(chat)
             return await message.chat.send(groupinfo)
 
+        elif message.content.startswith("/d group leave "):
+            chat_id = "".join(message.content.split(" ")[3::])
+            if not chat_id.isdigit():
+                return await message.chat.send("❌ *چت آیدی وارد شده نامعتبر است*")
+
+            try:
+                await self.bot.leave_chat(chat_id)
+            except bale.BaleError as err:
+                return await message.chat.send("❌ درخواست انجام نشد\n{}".format(err))
+            else:
+                await message.chat.send("🟢 *درخواست با موفقیت برای سرور های بله ارسال شد*")
+                with self.bot.make_db() as connection:
+                    cursor = connection.cursor()
+                    cursor.execute("DELETE FROM chat WHERE chat_id = '{}'".format(chat_id))
+                    connection.commit()
+                    cursor.close()
+
         elif message.content.startswith("/d group send "):
             chat_id = "".join(message.content.split(" ")[3::])
             if not chat_id.isdigit():
                 return await message.chat.send("❌ *چت آیدی وارد شده نامعتبر است*")
 
-            await message.chat.send("💎 *لطفا تا 30 ثانیه دیگر پیام مورد نظر خود را ارسال کنید*")
+            await message.chat.send("💎 *لطفا تا 120 ثانیه دیگر پیام مورد نظر خود را ارسال کنید*")
             try:
-                msg: "bale.Message" = await self.bot.wait_for("developer_message", check = lambda m: m.author == message.author and m.chat == message.chat)
+                msg: "bale.Message" = await self.bot.wait_for("developer_message", check = lambda m: m.author == message.author and m.chat == message.chat, timeout = 120.0)
             except asyncio.TimeoutError:
                 return await message.chat.send("💡 متاستفانه ارسال نکردید")
             else:
                 sent_message = await self.bot.send_message(chat_id, msg.content)
-                return await msg.reply("🚀 *پیام با موفقیت ارسال شد*\n\nپیام با آیدی {0} بر روی سرور های بله و در گروهی با نام مستعار {1} و آیدی {2} قرار گرفت.".format(sent_message.message_id, sent_message.chat.title, sent_message.chat_id))
+                return await msg.reply("🚀 *پیام با موفقیت ارسال شد*\n\nپیام با آیدی {0} بر روی سرور های بله و در گروهی با آیدی {1} قرار گرفت.".format(sent_message.message_id, sent_message.chat_id))
 
         elif message.content == "/d user get":
             await message.chat.send("💎 *لطفا تا 30 ثانیه دیگر یک پیام از کاربر مورد نظر ارسال نمائید*")
