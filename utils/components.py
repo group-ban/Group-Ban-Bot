5edccf2af8fb0969f3484be64758522531985f4b
Kian Ahmadian <dev.kian.ahmadian@gmail.com>
2023-05-12 11:03:53 +0330

    Change components in `/setup` & `/help` command

--- utils/components.py
+++ utils/components.py
@@ -9,10 +9,10 @@ class Components:
         return C(inline_keyboards=[[InlineKeyboard("داکیومنت دستورات ربات", url="https://groupban.ir/commands")]])
 
     def setup_command(self):
-        return C(inline_keyboards=[[InlineKeyboard("ضد اسپم", callback_data="anti_spam"), InlineKeyboard("ضد لینک", callback_data="anti_link")], [InlineKeyboard("ضد منشن", callback_data="anti_mention"), InlineKeyboard("ضد کلمه", callback_data="anti_word")], [InlineKeyboard("ضد بازارسال", callback_data="anti_forward"), InlineKeyboard("پاسخگویی خودکار", callback_data="auto_answer")], InlineKeyboard("ذخیره", callback_data="exit")])
+        return C(inline_keyboards=[[InlineKeyboard("ضد اسپم", callback_data="anti_spam"), InlineKeyboard("ضد لینک", callback_data="anti_link")], [InlineKeyboard("ضد منشن", callback_data="anti_mention"), InlineKeyboard("ضد بازارسال", callback_data="anti_forward")], InlineKeyboard("ذخیره", callback_data="exit")])
 
     def site_and_support_buttons(self):
-        return C(inline_keyboards=[InlineKeyboard("اطلاع رسانی گروه بان", url="https://ble.ir/groupban"), [InlineKeyboard("سایت گروه بان", url="https://groupban.ir/"), InlineKeyboard("پشتیبانی ربات", url="https://groupban.ir/support")]])
+        return C(inline_keyboards=[InlineKeyboard("اطلاع رسانی گروه بان", url="https://ble.ir/groupban"), [InlineKeyboard("سایت گروه بان", url="https://groupban.ir/"), InlineKeyboard("پشتیبانی ربات", url="https://groupban.ir/support")], [InlineKeyboard("دعوت ربات به گروه", url="https://groupban.ir/invite")]])
 
     def about_command(self):
         return C(inline_keyboards=[[InlineKeyboard("گیت هاب گروه بان", url="https://groupban.ir/github")], InlineKeyboard("سایت پروژه بله در پایتون (python-bale-bot)", url="https://python-bale-bot.ir")])
