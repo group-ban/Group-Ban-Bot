a67b57dd09e8f4aa2b101e93d92fabdf0fe5476a
Kian Ahmadian <dev.kian.ahmadian@gmail.com>
2023-05-18 00:13:49 +0330

    Add `edited_message` handler for Filters

--- cogs/filter.py
+++ cogs/filter.py
@@ -12,7 +12,8 @@ class Filter:
 
     def setup(self):
         return {
-            self.when_send_message_in_group: "message"
+            self.when_send_message_in_group: "message",
+            self.when_message_edited_in_group: "edited_message"
         }
 
     def chat_member_have_rate_limit(self, message: "bale.Message", chat_id: int, user_id: int) -> bool:
@@ -50,19 +51,19 @@ class Filter:
             filters = cursor.fetchone()
             if not filters:
                 return self.bot.dispatch("unverified_message", message)
+            standard_content = self.bot.make_persian(message.content)
             (anti_spam, anti_link, anti_mention, anti_word, anti_forward, anti_code, auto_answer) = filters
-            cursor.execute("SELECT word, answer FROM auto_answer WHERE chat_id = '{}'".format(message.chat.chat_id))
-            auto_answer_dict = {word: answer for word, answer in cursor.fetchall()}
+            cursor.execute("SELECT answer FROM auto_answer WHERE chat_id = '{}' AND word = '{}'".format(message.chat.chat_id, standard_content))
+            auto_answer_result = cursor.fetchone()
             cursor.execute("SELECT word FROM bad_words WHERE chat_id = '{}'".format(message.chat.chat_id))
-            bad_words = [word for (word, ) in cursor.fetchall()]
-        standard_content = self.bot.make_persian(message.content)
+            bad_words = [word for (word,) in cursor.fetchall()]
 
         user_spam_state = self.chat_member_have_rate_limit(message, int(message.chat_id), int(message.author.user_id))
 
         if ( (anti_spam and user_spam_state) or
             (anti_link and (re.search("(?P<url>https?://\S+)", standard_content, flags=re.IGNORECASE) or re.search("(?P<url>ble.ir)", standard_content, flags=re.IGNORECASE)) ) or
             (anti_mention and re.search("(?<=@)\w+", standard_content)) or
-            (anti_forward and message.forward_from_message_id) or
+            (anti_forward and (message.forward_from_message_id or message.forward_from or message.forward_from_chat)) or
             (anti_code and len(standard_content) > 500) ):
             return await self.do_after_check_chat_member(message.delete, message)
 
@@ -72,7 +73,33 @@ class Filter:
                     await self.do_after_check_chat_member(message.delete, message)
                     break
 
-        if auto_answer and auto_answer_dict.get(standard_content):
-            return await self.do_after_check_chat_member(self.bot.send_message_without_convert, message, message.chat_id, auto_answer_dict.get(standard_content))
+        if auto_answer and auto_answer_result:
+            return await self.do_after_check_chat_member(self.bot.send_message_without_convert, message, message.chat_id, (message.author.mention or message.author.first_name) + "\n" + auto_answer_result[0])
 
         self.bot.dispatch("verified_message", message)
+
+    async def when_message_edited_in_group(self, message: bale.Message):
+        if not (message.content and message.author and message.author != self.bot.user):
+            return
+
+        with self.bot.make_db() as connection:
+            cursor = connection.cursor()
+            cursor.execute("SELECT anti_link, anti_mention, anti_word, anti_code FROM chat WHERE chat_id = '{}'".format(message.chat.chat_id))
+            filters = cursor.fetchone()
+            if not filters:
+                return
+            standard_content = self.bot.make_persian(message.content)
+            (anti_link, anti_mention, anti_word, anti_code) = filters
+            cursor.execute("SELECT word FROM bad_words WHERE chat_id = '{}'".format(message.chat.chat_id))
+            bad_words = [word for (word,) in cursor.fetchall()]
+
+        if ( (anti_link and (re.search("(?P<url>https?://\S+)", standard_content, flags=re.IGNORECASE) or re.search("(?P<url>ble.ir)", standard_content, flags=re.IGNORECASE)) ) or
+            (anti_mention and re.search("(?<=@)\w+", standard_content)) or
+            (anti_code and len(standard_content) > 500) ):
+            return await self.do_after_check_chat_member(message.delete, message)
+
+        if anti_word:
+            for word in bad_words:
+                if word in standard_content:
+                    await self.do_after_check_chat_member(message.delete, message)
+                    break
